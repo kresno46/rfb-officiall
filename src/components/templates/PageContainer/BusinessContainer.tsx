50f7e22b88c54a7609172f441e4b01bd98ff1211
kresno46 <kresnonugroho46@gmail.com>
2025-07-08 11:54:20 +0700

    legalitas

---
+++ src/components/templates/PageContainer/BusinessContainer.tsx
@@ -0,0 +1,16 @@
+interface BusinessContainerProps {
+    children: React.ReactNode;
+    title?: string;
+}
+
+export default function BusinessContainer({ children, title }: BusinessContainerProps) {
+    return (
+        <div className="bg-white border-8 text-center rounded-2xl py-10 border-amber-400 space-y-10">
+            <div className="text-3xl font-bold">
+                <h1>{title}</h1>
+            </div>
+            <hr />
+            {children}
+        </div>
+    );
+}
