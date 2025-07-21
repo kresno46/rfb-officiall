148449616ca99a3a4f162b09337809255840a927
Kresno Nugroho <kresnonugroho46@gmail.com>
2025-07-21 12:38:43 +0700

    update api

--- src/pages/api/berita.ts
+++ src/pages/api/berita.ts
@@ -16,7 +16,7 @@ type Berita = {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
-        const response = await fetch("http://rfbdev.newsmaker.id/api/berita");
+        const response = await fetch("http://rfbdev.newsmaker.id/berita");
         if (!response.ok) {
             return res.status(response.status).json({ error: 'Failed to fetch berita' });
         }
--- src/pages/api/jfx.ts
+++ src/pages/api/jfx.ts
@@ -15,7 +15,7 @@ type Jfx = {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
-        const response = await fetch("http://rfbdev.newsmaker.id/api/jfx");
+        const response = await fetch("http://rfbdev.newsmaker.id/jfx");
         if (!response.ok) {
             return res.status(response.status).json({ error: 'Failed to fetch JFX Product' });
         }
