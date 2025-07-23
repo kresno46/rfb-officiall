75608a3c33b60d0b46423efd7728557b7ba46965
Kresno Nugroho <kresnonugroho46@gmail.com>
2025-07-23 10:44:54 +0700

    update:api

--- src/pages/api/spa.ts
+++ src/pages/api/spa.ts
@@ -15,7 +15,7 @@ type Spa = {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
-        const response = await fetch("http://rfb-backpanel.test/api/spa");
+        const response = await fetch("https://rfbdev.newsmaker.id/api/spa");
         if (!response.ok) {
             return res.status(response.status).json({ error: 'Failed to fetch SPA Product' });
         }
